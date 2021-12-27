d8baf861dd0f152dad5cb53b01244b08d3e68b95
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-12-27 13:32:56 +1000

    Refactored test

--- tests/unit/components/todo.spec.js
+++ tests/unit/components/todo.spec.js
@@ -1,6 +1,11 @@
 import { mount, shallowMount } from "@vue/test-utils";
 import Todo from "@/components/Todo";
 
+const addTodo = async (wrapper, text) => {
+  await wrapper.find("input").setValue(text);
+  await wrapper.find("form").trigger("submit"); // button click doesnt submit?
+};
+
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
@@ -17,11 +22,7 @@ describe("HelloWorld.vue", () => {
       attachToDocument: true,
     });
 
-    await wrapper
-      .find("input")
-      .setValue("Learn how to unit test vuejs component");
-
-    await wrapper.find("form").trigger("submit"); // button click doesnt submit?
+    await addTodo(wrapper, "Learn how to unit test vuejs component");
 
     expect(wrapper.findAll("ul > li").length).toBe(1);
     expect(wrapper.find("ul > li").text()).toBe(
@@ -33,14 +34,8 @@ describe("HelloWorld.vue", () => {
       props: { msg: "" },
       attachToDocument: true,
     });
-
-    await wrapper
-      .find("input")
-      .setValue("Learn how to unit test vuejs component");
-    await wrapper.find("form").trigger("submit");
-
-    await wrapper.find("input").setValue("K8s refresher");
-    await wrapper.find("form").trigger("submit");
+    await addTodo(wrapper, "Learn how to unit test vuejs component");
+    await addTodo(wrapper, "K8s refresher");
 
     expect(wrapper.findAll("ul > li").length).toBe(2);
 
